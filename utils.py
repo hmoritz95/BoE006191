8457960c30d3b1254b1ad397fe12e3438a1776b3
Hannes Moritz <hannes-moritz@gmx.de>
2022-02-26 22:42:46 +0000

    Included the second data sheet as well

--- utils.py
+++ utils.py
@@ -7,10 +7,17 @@ def import_data(path):
     :param path: string, the filepath
     :return: pd.DataFrame, stacked with Year, Metric
     """
-    data = pd.read_excel(path, index_col=0, header=[0, 1])
-    data.index.names = ['Company']
-    data.columns.names = ['Metrics', 'Year']
-    data = data.unstack(level=1)
-    data = data.unstack(level='Metrics')
-    data = data.sort_index(level='Year')
-    return data
+    data_general = pd.read_excel(path, index_col=0, header=[0, 1], sheet_name='Dataset 1 - General')
+    data_general.index.names = ['Company']
+    data_general.columns.names = ['Metrics', 'Year']
+    data_general = data_general.unstack(level=1)
+    data_general = data_general.unstack(level='Metrics')
+    data_general = data_general.sort_index(level='Year')
+
+    data_underwritings = pd.read_excel(path, index_col=0, header=[0, 1], sheet_name='Dataset 2 - Underwriting')
+    data_underwritings.index.names = ['Company']
+    data_underwritings.columns.names = ['Metrics', 'Year']
+    data_underwritings = data_underwritings.unstack(level=1)
+    data_underwritings = data_underwritings.unstack(level='Metrics')
+    data_underwritings = data_underwritings.sort_index(level='Year')
+    return data_general, data_underwritings
