c754363c0f03fa7effb4c6f2cb134bba5896e865
Hannes Moritz <hannes-moritz@gmx.de>
2022-02-26 22:24:20 +0000

    Implemented the import_data function in order to import the data

---
+++ utils.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+
+def import_data(path):
+    """
+    This function is intended to read in the excel file and to clean it up
+    :param path: string, the filepath
+    :return: pd.DataFrame, stacked with Year, Metric
+    """
+    data = pd.read_excel(path, index_col=0, header=[0, 1])
+    data.index.names = ['Company']
+    data.columns.names = ['Metrics', 'Year']
+    data = data.unstack(level=1)
+    data = data.unstack(level='Company')
+    data = data.swaplevel()
+    data = data.sort_index(level='Year')
+    return data
