7e243a17f772cd0d4c9b19d78b4d6857c336592c
Hannes Moritz <hannes-moritz@gmx.de>
2022-02-27 00:32:56 +0000

    Implemented the clean_values function

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 
 def import_data(path):
@@ -21,3 +22,26 @@ def import_data(path):
     data_underwritings = data_underwritings.unstack(level='Metrics')
     data_underwritings = data_underwritings.sort_index(level='Year')
     return data_general.replace(0, 0.01), data_underwritings.replace(0, 0.01)
+
+
+def clean_values(data, replacing_values):
+    """
+    This function aims to cleanup the data based on certain conditions and returning the cleaned data afterwards
+    :param data: pd.DataFrame, the original data
+    :param replacing_values: dict, the dictionary with the columns as keys and the checking value as value with the first one being < | >
+    :return: pd.DataFrame, returns the cleaned up data frame object
+    """
+    for i in replacing_values:
+        correction_term = replacing_values[i] #correction_term[2:]
+
+        if correction_term[1] == '<':
+            # choosing here the "biased" mean on purpose to keep at least the directions of the all-in values
+            temp = data[data[i] <= float(correction_term[1:])]
+            temp[i] = np.mean(data[i])
+            data.loc[temp.index, temp.columns] = temp
+        else:
+            # choosing here the "biased" mean on purpose to keep at least the directions of the all-in values
+            temp = data[data[i] >= float(correction_term[1:])]
+            temp[i] = np.mean(data[i])
+            data.loc[temp.index, temp.columns] = temp
+        del temp
